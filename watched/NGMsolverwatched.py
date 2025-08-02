3a5554e63f3af9a37d661e0dd08c962d88a58398
akaDryyy <drydsmv@gmail.com>
2025-08-02 18:50:49 +0200

    quag bug fix

--- watched/NGMsolverwatched.py
+++ watched/NGMsolverwatched.py
@@ -71,6 +71,7 @@ with open(players_path, 'r') as file:
         # Check aliases
         elif player_key in aliases:
             main_name = aliases[player_key]
+            main_name = main_name.strip().lower()
             if main_name in ranks:
                 players[player] = ranks[main_name]
             else:
